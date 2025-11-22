8226cba98aa8275e6cd713be3b3edeee686f64c7
spread <riccalira95@protonmail.com>
2025-11-22 16:19:23 +0100

    feat: update build script

--- build.sh
+++ build.sh
@@ -6,15 +6,15 @@ rm ./bin/*
 
 # Per Linux
 GOOS=linux GOARCH=amd64 go build -o "./bin/perfect-menu_print_orders-amd64-linux" "./src/"
-shasum -a 256 "./bin/perfect-menu_print_orders-amd64-linux" > "./bin/perfect-menu_print_orders-amd64-linux.sha256"
-gzip -k -f "./bin/perfect-menu_print_orders-amd64-linux"
+tar -czf "./bin/perfect-menu_print_orders-amd64-linux.tar.gz" -C ./bin perfect-menu_print_orders-amd64-linux -C .. templates/
+shasum -a 256 "./bin/perfect-menu_print_orders-amd64-linux.tar.gz" > "./bin/perfect-menu_print_orders-amd64-linux.tar.gz.sha256"
 
 # For Raspberry Pi (ARM)
 GOOS=linux GOARCH=arm64 go build -o "./bin/perfect-menu_print_orders-arm64-linux" "./src/"
-shasum -a 256 "./bin/perfect-menu_print_orders-arm64-linux" > "./bin/perfect-menu_print_orders-arm64-linux.sha256"
-gzip -k -f "./bin/perfect-menu_print_orders-arm64-linux" 
+tar -czf "./bin/perfect-menu_print_orders-arm64-linux.tar.gz" -C ./bin perfect-menu_print_orders-arm64-linux -C .. templates/
+shasum -a 256 "./bin/perfect-menu_print_orders-arm64-linux.tar.gz" > "./bin/perfect-menu_print_orders-arm64-linux.tar.gz.sha256" 
 
 # Per macOS
 GOOS=darwin GOARCH=amd64 go build -o "./bin/perfect-menu_print_orders-amd64-macos" "./src/"
-shasum -a 256 "./bin/perfect-menu_print_orders-amd64-macos" > "./bin/perfect-menu_print_orders-amd64-macos.sha256"
-gzip -k -f "./bin/perfect-menu_print_orders-amd64-macos"
+tar -czf "./bin/perfect-menu_print_orders-amd64-macos.tar.gz" -C ./bin perfect-menu_print_orders-amd64-macos -C .. templates/
+shasum -a 256 "./bin/perfect-menu_print_orders-amd64-macos.tar.gz" > "./bin/perfect-menu_print_orders-amd64-macos.tar.gz.sha256"
